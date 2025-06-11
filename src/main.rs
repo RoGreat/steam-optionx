232998badc517e1e917b41918be6533a64382737
RoGreat <roguegreat@gmail.com>
2025-06-10 19:44:29 -0500

    Move slint files to ui

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,14 @@
-use slint::{Model, ModelExt, ModelRc, SharedString, StandardListViewItem, VecModel};
-use std::rc::Rc;
+use slint;
 use webbrowser;
 
-slint::slint! {
-    import { VerticalBox, ListView, HorizontalBox, LineEdit, Button, StyleMetrics } from "std-widgets.slint";
-
-    export global HyperlinkClicked {
-        pure callback link-clicked(string);
-    }
-
-    export component Hyperlink inherits Text {
-        in property<string> url;
-
-        area := TouchArea {
-            clicked => { HyperlinkClicked.link-clicked(root.url) }
-        }
-    }
-
-    export component TableViewPage inherits Window {
-        property <int> sort-index: -1;
-        property <bool> sort-ascending;
-        callback file-dialog;
-        callback reload-file;
-
-        HorizontalBox {
-            VerticalLayout {
-                spacing: StyleMetrics.layout-spacing;
-
-                VerticalLayout {
-                    Text {
-                        text: @tr("Open 'Steam/userdata/XXXXXXXX/config/localconfig.vdf':");
-                    }
-
-                    GridLayout {
-                        HorizontalLayout {
-                            spacing: StyleMetrics.layout-spacing;
-
-                            file_path := LineEdit {
-                                accepted => { root.reload-file() }
-                            }
-
-                            Button {
-                                text: "Open file...";
-                                clicked => { root.file-dialog() }
-                            }
-                        }
-                    }
-                }
-
-                VerticalLayout {
-                    Text {
-                        text: @tr("Filter by game:");
-                    }
-
-                    filter-edit := LineEdit {
-                        placeholder-text: @tr("Enter game...");
-                    }
-
-                    Hyperlink {
-                        text: @tr("Game");
-                        url: @tr("https://example.com");
-                    }
-                }
-            }
-        }
-    }
-}
+slint::include_modules!();
 
 fn main() {
-    let app = TableViewPage::new().unwrap();
+    let sox = SteamOptionX::new().unwrap();
 
-    app.global::<HyperlinkClicked>().on_link_clicked(|url| {
+    sox.global::<LinkHandler>().on_link_clicked(|url| {
         let _ = webbrowser::open(url.as_str());
     });
 
-    app.run().unwrap();
+    sox.run().unwrap();
 }
