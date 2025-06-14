c6b43f868361ce484dce126756a2a166d0ef660f
RoGreat <roguegreat@gmail.com>
2025-06-14 18:14:15 -0500

    Try egui once more

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "A basic Rust devshell for NixOS users developing Leptos";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -9,65 +7,51 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       rust-overlay,
       flake-utils,
-      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       with pkgs;
       {
         devShells.default = mkShell {
-          nativeBuildInputs = [
+          packages = [
+            openssl
             pkg-config
-            gobject-introspection
-            cargo
-            cargo-tauri
-            nodejs
+            (rust-bin.selectLatestNightlyWith (
+              toolchain:
+              toolchain.default.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                ];
+              }
+            ))
           ];
 
-          buildInputs =
-            [
-              at-spi2-atk
-              atkmm
-              cacert
-              cairo
-              cargo-make
-              gcc
-              gdk-pixbuf
-              glib
-              gtk3
-              harfbuzz
-              librsvg
-              libsoup_3
-              openssl
-              pango
-              pkg-config
-              trunk
-              webkitgtk_4_1
-              (rust-bin.selectLatestNightlyWith (
-                toolchain:
-                toolchain.default.override {
-                  extensions = [
-                    "rust-src"
-                    "rust-analyzer"
-                  ];
-                  targets = [ "wasm32-unknown-unknown" ];
-                }
-              ))
-            ]
-            ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
-              darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${
+              lib.makeLibraryPath [
+                libxkbcommon
+                libGL
 
-          shellHook = '''';
+                # WINIT_UNIX_BACKEND=wayland
+                wayland
+
+                # WINIT_UNIX_BACKEND=x11
+                xorg.libXcursor
+                xorg.libXrandr
+                xorg.libXi
+                xorg.libX11
+              ]
+            }
+          '';
         };
       }
     );
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,109 @@
-mod renderer;
-mod tailwind;
+#![windows_subsystem = "windows"]
 
-use crate::renderer::*;
-use ::chrono::NaiveDate;
-use leptos::prelude::*;
-use leptos_struct_table::*;
-use std::ops::Range;
-use tailwind::TailwindClassesPreset;
+mod api;
+mod vdf;
 
-// This generates the component BookTable
-#[derive(TableRow, Clone, Debug)]
-#[table(classes_provider = "TailwindClassesPreset")]
-pub struct Book {
-    pub id: u32,
-    #[table(renderer = "InputCellRenderer")]
-    pub title: String,
-    #[table(renderer = "InputCellRenderer")]
-    pub author: String,
-    pub publish_date: NaiveDate,
+use directories::BaseDirs;
+use eframe::egui;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+use std::path::PathBuf;
+
+fn main() -> eframe::Result {
+    let config: Config = confy::load("steam-optionx", None).unwrap();
+    let picked_path = Some(config.steam_config);
+    let appids = Some(vdf::appids(picked_path.clone().unwrap()).unwrap());
+    let game_names = Some(api::get_game_names().unwrap());
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 240.0])
+            .with_drag_and_drop(true),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Steam OptionX",
+        options,
+        Box::new(|_cc| {
+            Ok(Box::new(EguiApp {
+                picked_path: picked_path,
+                appids: appids,
+                game_names: game_names,
+            }))
+        }),
+    )
 }
 
-impl TableDataProvider<Book> for RwSignal<Vec<Book>> {
-    async fn get_rows(&self, _: Range<usize>) -> Result<(Vec<Book>, Range<usize>), String> {
-        let books = self.get_untracked().to_vec();
-        let len = books.len();
-        Ok((books, 0..len))
-    }
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Config {
+    steam_config: String,
+}
 
-    async fn row_count(&self) -> Option<usize> {
-        Some(self.get_untracked().len())
+fn userdata() -> PathBuf {
+    match env::consts::OS {
+        "windows" => PathBuf::from(r"C:\Program Files (x86)\Steam\userdata"),
+        _ => BaseDirs::new()
+            .unwrap()
+            .data_dir()
+            .to_path_buf()
+            .join("Steam/userdata"),
     }
 }
 
-fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
-    console_error_panic_hook::set_once();
+#[derive(Default)]
+struct EguiApp {
+    picked_path: Option<String>,
+    appids: Option<Vec<String>>,
+    game_names: Option<HashMap<String, String>>,
+}
 
-    mount_to_body(|| {
-        let rows = RwSignal::new(vec![
-            Book {
-                id: 1,
-                title: "The Great Gatsby".to_string(),
-                author: "F. Scott Fitzgerald".to_string(),
-                publish_date: NaiveDate::from_ymd_opt(1925, 4, 10).unwrap(),
-            },
-            Book {
-                id: 2,
-                title: "The Grapes of Wrath".to_string(),
-                author: "John Steinbeck".to_string(),
-                publish_date: NaiveDate::from_ymd_opt(1939, 4, 14).unwrap(),
-            },
-            Book {
-                id: 3,
-                title: "Nineteen Eighty-Four".to_string(),
-                author: "George Orwell".to_string(),
-                publish_date: NaiveDate::from_ymd_opt(1949, 6, 8).unwrap(),
-            },
-            Book {
-                id: 4,
-                title: "Ulysses".to_string(),
-                author: "James Joyce".to_string(),
-                publish_date: NaiveDate::from_ymd_opt(1922, 2, 2).unwrap(),
-            },
-        ]);
+impl eframe::App for EguiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Find 'Steam/userdata/########/config/localconfig.vdf'");
 
-        let on_change = move |evt: ChangeEvent<Book>| {
-            rows.write()[evt.row_index] = evt.changed_row.get_untracked();
-        };
+            if ui.button("Open file…").clicked() {
+                if let Some(path) = rfd::FileDialog::new()
+                    .add_filter("text", &["vdf"])
+                    .set_directory(userdata())
+                    .pick_file()
+                {
+                    self.picked_path = Some(path.display().to_string());
+                    let config = Config {
+                        steam_config: self.picked_path.clone().unwrap(),
+                    };
+                    confy::store("steam-optionx", None, config).unwrap();
+                    self.appids = Some(vdf::appids(self.picked_path.clone().unwrap()).unwrap());
+                    println!("{:?}", self.appids);
+                }
+            }
 
-        view! {
-            <div class="rounded-md overflow-clip m-10 border dark:border-gray-700 w-[50%]"
-                .to_string()>
-                <table class="text-sm text-left text-gray-500 dark:text-gray-400 mb-[-1px]">
-                    <TableContent rows on_change scroll_container="html" />
-                </table>
-            </div>
+            if let Some(picked_path) = &self.picked_path {
+                ui.horizontal(|ui| {
+                    ui.label("Picked file:");
+                    ui.monospace(picked_path);
+                });
+            }
 
-            <pre>{move || format!("{:#?}", rows.get())}</pre>
-        }
-    })
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                egui::Grid::new("games").show(ui, |ui| {
+                    if let Some(appids) = &self.appids {
+                        if let Some(game_names) = &self.game_names {
+                            for appid in appids.iter() {
+                                let game_name = game_names.get(appid);
+                                if let Some(game_name) = game_name {
+                                    ui.hyperlink_to(
+                                        game_name,
+                                        "https://store.steampowered.com/app/".to_owned() + appid,
+                                    );
+                                    ui.end_row();
+                                }
+                            }
+                        }
+                    }
+                });
+            });
+        });
+    }
 }
